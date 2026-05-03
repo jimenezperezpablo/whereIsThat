12a31f9ca36752efd52c4c42af79ed26a17b88b0
jimenezperezpablo <jimenezperezpablo001.alu@stemgranada.com>
2026-05-03 18:42:42 +0200

    feat: Proyecto Meaven

---
+++ src/main/java/com/PabloJimenez07/whereIsThat/WhereIsThatApplication.java
@@ -0,0 +1,13 @@
+package com.PabloJimenez07.whereIsThat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereIsThatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhereIsThatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PabloJimenez07/whereIsThat/WhereIsThatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PabloJimenez07.whereIsThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereIsThatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
